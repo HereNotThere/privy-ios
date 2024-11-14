c864300ebfab2aaeb0289383ee31c1c4bafac7b1
Erik Olsson <valross2@gmail.com>
2024-11-14 16:28:21 +0100

    add maccatalyst platform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Privy",
     platforms: [
-        .iOS(.v13),
+      .iOS(.v13),
+      .macCatalyst(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
